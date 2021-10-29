bb7afb014ed8720a1d841d923427b07f8d7bb6be
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-10-29 12:41:18 -0700

    pass the sixth test

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 //Business Logic
 
 function beepBoop(value){
-  let answerArray = [];
-  for(let index = 0; index <= value; index++){
-    let indexArray = index.toString().split("").sort().reverse() 
-    for(let i = 0; i < indexArray.length; i++){
-      if(indexArray.includes("3")){
-        answerArray.push("Won't you be my neighbor?");
-        break;
-      }
-      else if(indexArray.includes("2")){
-        answerArray.push("Boop!");
-        break;
-      }
-      else if(indexArray.includes("1")){
-        answerArray.push("Beep!")
-        break;
-      }
-      else{
-        answerArray.push(index);
-        break;
+  if(!Number.isInteger(value)){
+    return "Error please input an integer!"
+  }
+  else{
+    let answerArray = [];
+    for(let index = 0; index <= value; index++){
+      let indexArray = index.toString().split("").sort().reverse() 
+      for(let i = 0; i < indexArray.length; i++){
+        if(indexArray.includes("3")){
+          answerArray.push("Won't you be my neighbor?");
+          break;
+        }
+        else if(indexArray.includes("2")){
+          answerArray.push("Boop!");
+          break;
+        }
+        else if(indexArray.includes("1")){
+          answerArray.push("Beep!")
+          break;
+        }
+        else{
+          answerArray.push(index);
+          break;
+        }
       }
     }
+    return answerArray;
   }
-  return answerArray;
+  
 }
 
 //UI Logic
