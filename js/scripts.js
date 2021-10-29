d902200b6fa0aa54b95865b2ccd978233c9e2729
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-10-29 10:27:38 -0700

    pass the third test

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,12 @@ function beepBoop(value){
     if(index === 1){
       answerArray.push("Beep!");
     }
+    else if(index === 2){
+      answerArray.push("Boop!");
+    }
+    else if(index === 3){
+      answerArray.push("Won't you be my neighbor?")
+    }
     else{
       answerArray.push(index);
     }
