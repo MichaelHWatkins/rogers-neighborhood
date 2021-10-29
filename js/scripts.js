3b587b0284cc821abcdfa890bd253d86815f3403
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-10-29 10:18:29 -0700

    pass the second test

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 //Business Logic
 
 function beepBoop(value){
-  return value
+  let answerArray = [];
+  for(let index = 0; index <= value; index++){
+    if(index === 1){
+      answerArray.push("Beep!");
+    }
+    else{
+      answerArray.push(index);
+    }
+  }
+  return answerArray;
 }
 
 //UI Logic
